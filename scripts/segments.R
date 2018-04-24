8cc6441b453028d45a5c2dd811c7933bb76f30e2
jonataseduardo <jonataseduardo@gmail.com>
2018-04-24 11:06:36 -0300

    organizing code

--- scripts/segments.R
+++ scripts/segments.R
@@ -1,20 +1,21 @@
 library(data.table)
 library(splitstackshape)
 library(diverdt)
+library(ggplot)
 
 seg_l <- 
   unlist(
     lapply(
       readLines('../data/segments/EUR/chro1.segs'),
-      function(line_) strsplit(line_, " ")
+      function(line_) lapply(strsplit(line_, " "), as.integer)
       )
     )
 
-seg_dt <- data.table(CHR = 1, WD_POS_ID = seg_l)
-seg_dt[, WD_ID := as.integer(WD_POS_ID)]
+# Using plus one since R vectors start in one
+# Ask Florencia why her index array starts in 0
+seg_dt <- data.table(CHR = 1, WD_ID = seg_l + 1) 
 seg_dt[, NREP := shift(WD_ID, type='lead') -  WD_ID]
 wd_dt <- expandRows(seg_dt[!is.na(NREP)], 'NREP')
-           
 
 eur_dt <- load_bim_frq('../data/reich_data/EUR/EUR', 
                        one_allele_perline = FALSE)
@@ -23,14 +24,14 @@ recomb_data <-
   fread('../data/genetic_map_b36/genetic_map_chr1_b36.txt')
 names(recomb_data) <- c('POS', 'RATE', 'CM_R')
 
-recomb_data
-
-eur_dt[CHR == 1, WD_ID := wd_dt$WD_ID]
-
 eur_dt1 <- eur_dt[CHR == 1]
-eur_dt1[, HTZ := MAF * ( 1 - MAF)]
 
-reur1 <- recomb_data[eur_dt1, on = .(POS)]
-reur1[!is.na(RATE)]
+eur_dt1[, `:=`(POS_ID = .I, 
+               WD_ID = wd_dt$WD_ID,
+               HTZ = MAF * ( 1 - MAF))]
+eur_dt1[, HTZ_WD := mean(HTZ), by = WD_ID]
 
-eur_dt1[, .(HTZ_WD = mean(HTZ)), by = WD_ID]
+reur1 <- recomb_data[eur_dt1, on = .(POS)
+                     ][!is.na(RATE)]
+
+reur1[WD_ID == POS_ID]

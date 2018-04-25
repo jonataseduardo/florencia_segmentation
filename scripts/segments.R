5a9c8a38904ad4b1fc94f4ce679003904afdd669
jonataseduardo <jonataseduardo@gmail.com>
2018-04-25 10:47:21 -0300

    continuing

--- scripts/segments.R
+++ scripts/segments.R
@@ -35,19 +35,24 @@ setnames(seg_dt,
          c("IL", "IR", "WD_SIZE", "AVG_HOM"))
 
 
+eur_segs_ref <-
+  fread(paste0(files_path, '/EURmap'), 
+        select = 1:3,
+        col.names = c("CHR", "POS", "SNP"))
 
+eur_dt <- 
+  load_bim_frq('../data/reich_data/EUR/EUR', 
+               one_allele_perline = FALSE)
 
-seg_dt[, NREP := shift(WD_ID, type='lead') -  WD_ID]
-wd_dt <- expandRows(seg_dt[!is.na(NREP)], 'NREP')
-
-eur_dt <- load_bim_frq('../data/reich_data/EUR/EUR', 
-                       one_allele_perline = FALSE)
+eur_dt1 <- 
+  eur_dt[eur_segs_ref, on = .(CHR, POS, SNP)] 
 
 recomb_data <- 
-  fread('../data/genetic_map_b36/genetic_map_chr1_b36.txt')
-names(recomb_data) <- c('POS', 'RATE', 'CM_R')
+  fread('../data/genetic_map_b36/genetic_map_chr1_b36.txt',
+        col.names = c('POS', 'RATE', 'CM_R'))
 
-eur_dt1 <- eur_dt[CHR == 1]
+recomb_data
+
 
 eur_dt1[, `:=`(POS_ID = .I, 
                WD_ID = wd_dt$WD_ID,
